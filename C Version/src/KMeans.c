581386b26a6b7aff82e9711fa11f89f448ceabee
deivedwilliam <deived.william@live.com>
2019-09-11 23:32:29 -0500

    kmeans_fix

--- C Version/src/KMeans.c
+++ C Version/src/KMeans.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "../include/Exception.h"
 #include <math.h>
+#include <string.h>
+#include <time.h>
 
 #define CKMeans(Obj) ((KMeansStructure)Obj)
 
@@ -58,11 +60,21 @@ KMeans InitKMeans(int centroids, float tolerance, int max_iteractions)
 #ifdef RAND_CENTROID_POINTS
         srand(time(NULL));
         km->initial_centroids = (Pointer)calloc(km->centroids, sizeof(struct Point));
-        for(i = 0; i < km->centroids; i++)
+        for(int i = 0; i < km->centroids; i++)
         {
-            ((Point)km->initial_centroids)[i].x = rand()%10;
-            ((Point)km->initial_centroids)[i].y = rand()%10;
+            ((Point)km->initial_centroids)[i].x = -rand()%20 + rand()%20;
+            ((Point)km->initial_centroids)[i].y = -rand()%20 + rand()%20;
 
+        }
+#endif
+#ifdef DEBUG_ON
+
+        puts("Initial Centroids");
+        for(int i = 0; i < km->centroids; i++)
+        {
+            printf("Cenroid [%i] -> x = %f", i, ((Point)km->initial_centroids)[i].x);
+            printf("\tCenroid [%i] -> y = %f", i, ((Point)km->initial_centroids)[i].y);
+            puts(" ");
         }
 #endif
     }
@@ -94,15 +106,20 @@ void fit(KMeans* km, Pointer data, int data_num)
     int clusterFound;
     int centroid;
     int sum_counter = 0;
-
+    int* clusters_prev = NULL;
     CKMeans(km)->dataPointer = data;
     CKMeans(km)->data_num = data_num;
 
+
     try
     {
+        if(NULLReference(km))
+        {
+            throw(__NullPointerException__);
+        }
         CKMeans(km)->clusters = (int*)calloc(data_num, sizeof(int));
-        
-        if(NULLReference(CKMeans(km)->clusters))
+        clusters_prev = (int*)calloc(data_num, sizeof(int));
+        if(NULLReference(CKMeans(km)->clusters) | NULLReference(clusters_prev))
         {
             throw(__MemoryAllocationException__);
         }
@@ -111,49 +128,73 @@ void fit(KMeans* km, Pointer data, int data_num)
     {
         PrintExceptionStdOut(MemoryAllocationException);
     }
-
-    for(i = 0; i < CKMeans(km)->data_num; i++)
+    catch(NullPointerException)
     {
-        p = ((Point)CKMeans(km)->dataPointer);
-
-        currenteDist = CKMeans(km)->calcDistanceFunction(p, &(((Point)CKMeans(km)->initial_centroids)[0]));
-
-        for(cluster = 1; cluster < CKMeans(km)->centroids; cluster++)
-        {
-            distance = CKMeans(km)->calcDistanceFunction(p, &(((Point)CKMeans(km)->initial_centroids)[cluster]));
-
-            if(distance < currenteDist)
-            {
-                currenteDist = distance;
-                clusterFound = cluster;
-            } 
-        }
-
-        CKMeans(km)->clusters[i] = clusterFound;
+        PrintExceptionStdOut(NullPointerException);
     }
-
-    for(centroid = 0; centroid < CKMeans(km)->centroids; centroid++)
+    p = ((Point)CKMeans(km)->dataPointer);
+    while(1)
     {
+        memcpy(clusters_prev, CKMeans(km)->clusters, CKMeans(km)->data_num*sizeof(int));
 
         for(i = 0; i < CKMeans(km)->data_num; i++)
         {
-            if(CKMeans(km)->clusters[i] == centroid)
+            currenteDist = CKMeans(km)->calcDistanceFunction(&p[i], &(((Point)CKMeans(km)->initial_centroids)[0]));
+          
+            for(cluster = 1; cluster < CKMeans(km)->centroids; cluster++)
             {
-                p = ((Point)CKMeans(km)->dataPointer);
-                sum.x += p[i].x;
-                sum.y += p[i].y;
-                sum_counter += 1;
+                distance = CKMeans(km)->calcDistanceFunction(&p[i], &(((Point)CKMeans(km)->initial_centroids)[cluster]));
+
+                if(distance < currenteDist)
+                {
+                    currenteDist = distance;
+                    clusterFound = cluster;
+                } 
             }
 
+            CKMeans(km)->clusters[i] = clusterFound;
+        }
+        
+        for(centroid = 0; centroid < CKMeans(km)->centroids; centroid++)
+        {
+
+            for(i = 0; i < CKMeans(km)->data_num; i++)
+            {
+                if(CKMeans(km)->clusters[i] == centroid)
+                {
+                    sum.x += p[i].x;
+                    sum.y += p[i].y;
+                    sum_counter += 1;
+                }
+
+            }
+
+            if(sum_counter > 0)
+            {
+                sum.x /= sum_counter;
+                sum.y /= sum_counter;
+                ((Point)CKMeans(km)->initial_centroids)[centroid].x = sum.x;
+                ((Point)CKMeans(km)->initial_centroids)[centroid].y = sum.y;
+                sum_counter = 0;
+            }
         }
 
-        if(sum_counter > 0)
+        if(memcmp(clusters_prev, CKMeans(km)->clusters, CKMeans(km)->data_num*sizeof(int)) == 0)
         {
-            sum.x /= sum_counter;
-            sum.y /= sum_counter;
-            ((Point)CKMeans(km)->initial_centroids)->x = sum.x;
-            ((Point)CKMeans(km)->initial_centroids)->y = sum.y;
-            sum_counter = 0;
+            break;
         }
     }
+#ifdef DEBUG_ON
+    for(i = 0; i < CKMeans(km)->data_num; i++)
+    {
+        printf("[%i]\n", CKMeans(km)->clusters[i]);
+    }
+
+    puts("Final Centroids");
+    for(i = 0; i < CKMeans(km)->centroids; i++)
+    {
+        printf("Cenroid [%i] -> x = %f", i, ((Point)CKMeans(km)->initial_centroids)[i].x);
+        printf("\tCenroid [%i] -> y = %f\n", i, ((Point)CKMeans(km)->initial_centroids)[i].y);
+    }
+#endif    
 }
